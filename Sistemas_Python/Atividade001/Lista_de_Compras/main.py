0cbb56f3a77a7c675d4e56f7d6631305a0152ef6
unknown <novaislais1@gmail.com>
2024-06-21 12:56:54 -0300

    LNovais08

--- Sistemas_Python/Atividade001/Lista_de_Compras/main.py
+++ Sistemas_Python/Atividade001/Lista_de_Compras/main.py
@@ -25,16 +25,16 @@ def main(page: Page):
         cursor.execute("SELECT MAX(idLista) FROM compras")
         result = cursor.fetchone()
         if result[0] is None:
-            id_lista = "LISTA1"
+            id_lista = 0
         else:
-            id_lista_num = int(result[0].replace("LISTA", ""))
-            id_lista = f"{id_lista_num + 1}"
+            id_lista = int(result[0]) + 1
         conn.close()
         return id_lista
 
     # Contador de itens
     cont = 0
     id_lista = get_current_id_lista()
+    item_list = []
 
     # Função para adicionar item à lista
     def add_item(e):
@@ -43,13 +43,67 @@ def main(page: Page):
         if item:
             # Incrementa o contador e adiciona o item na lista
             cont += 1
-            items.controls.append(ft.Text(f"{cont}. {item}", size=20, color="black"))
+            item_list.append(item)
+            linhaLV = create_item_row(cont, item)
+            items.controls.append(linhaLV)
             compra.value = ""
             items.update()
             page.update()
         else:
             compra.error_text = "Item não pode ser vazio"
             compra.update()
+
+    def create_item_row(index, item):
+        item_text = ft.Text(f"{index}. {item}", size=20, color="black")
+        edit_button = ft.IconButton(icon=ft.icons.EDIT_ROUNDED, icon_color="green")
+        delete_button = ft.IconButton(icon=ft.icons.DELETE_SHARP, icon_color="red")
+
+        edit_button.on_click = lambda e: edit_item(e, index-1)
+        delete_button.on_click = lambda e: delete_item(e, index-1)
+
+        return ft.Row(
+            controls=[
+                item_text,
+                ft.Row(
+                    controls=[edit_button, delete_button],
+                    spacing=0
+                )
+            ],
+            alignment=ft.MainAxisAlignment.SPACE_AROUND
+        )
+
+    def delete_item(e, index):
+        item_list.pop(index)
+        refresh_list()
+
+    def edit_item(e, index):
+        def close_dlg(e):
+            edited_text = edit_text.value.strip()
+            if edited_text:
+                item_list[index] = edited_text
+                refresh_list()
+            dlg.open = False
+            page.update()
+
+        edit_text = ft.TextField(label="Digite aqui", value=item_list[index])
+        dlg = ft.AlertDialog(
+            actions=[
+                edit_text,
+                ft.TextButton(text="OK", on_click=close_dlg),
+            ],
+            actions_padding=10,
+        )
+        page.dialog = dlg
+        dlg.open = True
+        page.update()
+
+
+    def refresh_list():
+        items.controls.clear()
+        for i, item in enumerate(item_list):
+            items.controls.append(create_item_row(i + 1, item))
+        items.update()
+        page.update()
 
     # Função para finalizar a lista e salvar os itens no banco de dados
     def Fim(e):
@@ -58,12 +112,13 @@ def main(page: Page):
             # Conecta ao banco de dados e insere os valores
             conn = sqlite3.connect('Atividade001/Lista_de_Compras/db/compras.db')
             cursor = conn.cursor()
-            for control in items.controls:  # Pegar os itens da lista pelo for
-                item_value = control.value.split(". ", 1)[1]  # Separa o número do item
-                cursor.execute("INSERT INTO compras (idLista, nome) VALUES (?, ?)", (id_lista, item_value))  # Insere os itens no db
+            for item in item_list:  # Pegar os itens da lista pelo for
+                cursor.execute("INSERT INTO compras (idLista, nome) VALUES (?, ?)", (id_lista, item))  # Insere os itens no db
+
             conn.commit()
             conn.close()
             items.controls.clear()  # Limpa a lista
+            item_list.clear()
             items.update()
             page.update()
             cont = 0  # Reinicia o contador
@@ -116,6 +171,7 @@ def main(page: Page):
         alignment=ft.MainAxisAlignment.CENTER,  # Centralizando os contêineres na tela
         expand=True
     )
+
     page.add(principal)
 
 ft.app(target=main)
