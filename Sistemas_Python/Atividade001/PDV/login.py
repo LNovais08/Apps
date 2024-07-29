f34dde861eaf4d48799275212a0faadef7184f6a
unknown <novaislais1@gmail.com>
2024-07-29 14:04:03 -0300

    LNovais08

--- Sistemas_Python/Atividade001/PDV/login.py
+++ Sistemas_Python/Atividade001/PDV/login.py
@@ -9,7 +9,7 @@ def main(page: Page):
     page.title = "Página Login"
     # Centralizar a janela na tela
     page.window_center()
-    
+
     # Criando o contêiner principal
     principal = ft.Container(
         bgcolor=ft.colors.WHITE,
@@ -48,12 +48,11 @@ def main(page: Page):
         email_login = ft.TextField(label="E-mail", width=450, color="black", text_size=15)
         senha_login = ft.TextField(label="Senha", width=450, color="black", password=True, can_reveal_password=True, text_size=15)
         logar = ft.ElevatedButton(text="Logar", width=150, height=35, color="white", on_click=entrar)
-        link = ft.TextButton("Clique aqui para se Cadastrar", on_click=link_clicked)
 
         # Criando o contêiner de login
         login = ft.Container(
             content=ft.Column(
-                controls=[text_login, email_login, senha_login, logar, link],
+                controls=[text_login, email_login, senha_login, logar],
                 alignment=ft.MainAxisAlignment.CENTER,
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
                 spacing=20
@@ -65,103 +64,6 @@ def main(page: Page):
         )
         principal.content = ft.Row(
             controls=[img_container, login],
-            alignment=ft.MainAxisAlignment.CENTER,  # Centralizando os contêineres na tela
-            expand=True
-        )
-        page.add(principal)
-        page.update()
-
-    def link_clicked(e):
-        def cadastrar(e):
-            nome1 = Nome.value
-            tel1 = Tel.value
-            email1 = Email.value
-            sexo1 = sexo.value
-            senha1 = senha.value
-            adm1 = adm.value
-            if not email1:
-                Email.error_text = "Preencha seu E-mail"
-                page.update()
-            elif not senha1:
-                senha.error_text = "Preencha sua Senha"
-                page.update()
-            elif not nome1:
-                Nome.error_text = "Preencha seu Nome"
-                page.update()
-            elif not sexo1:
-                sexo.error_text = "Preencha qual o seu Sexo"
-                page.update()
-            elif not tel1:
-                Tel.error_text = "Preencha seu Telefone"
-                page.update()
-            else:
-                # Conecta ao banco de dados e insere os valores
-                conn = sqlite3.connect('PDV/db/cadastrosU.db')
-                cursor = conn.cursor()
-                cursor.execute("INSERT INTO cadastros_Usuarios (nome, Tel, email, sexo, grau, senha) VALUES (?, ?, ?, ?, ?, ?)", (nome1, tel1, email1, sexo1, adm1, senha1))
-                conn.commit()
-                conn.close()
-                Nome.value = ""
-                Tel.value = ""
-                Email.value = ""
-                sexo.value = ""
-                adm.value = ""
-                senha.value = ""
-                page.update()
-                       
-        # Função para formatar e validar o telefone
-        def format_telefone(e):
-            raw_tel = ''.join(filter(str.isdigit, Tel.value))[:11]  # Somente dígitos e max 11 caracteres
-            formatted_tel = raw_tel
-            if len(raw_tel) > 10:
-                formatted_tel = f"({raw_tel[:2]}) {raw_tel[2:7]}-{raw_tel[7:11]}"
-            elif len(raw_tel) > 6:
-                formatted_tel = f"({raw_tel[:2]}) {raw_tel[2:6]}-{raw_tel[6:]}"
-            elif len(raw_tel) > 2:
-                formatted_tel = f"({raw_tel[:2]}) {raw_tel[2:]}"
-            Tel.value = formatted_tel
-            page.update()
-
-        def dropdown_changed(e):
-            adm.value = adm.value
-            page.update()
-        
-        # Itens para Cadastro de Usuário
-        Nome = ft.TextField(label="Nome", width=450, color="black", text_size=15, border_color="gray")
-        Tel = ft.TextField(label="Telefone", width=450, color="black", text_size=15, border_color="gray", on_change=format_telefone)
-        Email = ft.TextField(label="E-mail", width=450, color="black", border_color="gray")
-        sexo = ft.RadioGroup(
-            content=ft.Row([
-                ft.Radio(value="Feminino", label="Feminino"),
-                ft.Radio(value="Masculino", label="Masculino")
-            ], alignment=ft.MainAxisAlignment.CENTER),
-        ) 
-        adm = ft.Dropdown(
-            width=450,
-            on_change=dropdown_changed,
-            options=[
-                ft.dropdown.Option("Admin"),
-                ft.dropdown.Option("Funcionário"),
-            ],
-        ) 
-        senha = ft.TextField(label="Senha", width=450, color="black", password=True, can_reveal_password=True, text_size=15, border_color="gray")
-        cadastrar_btn = ft.ElevatedButton(text="Cadastrar", width=150, height=35, color="white", on_click=cadastrar)
-
-        cadastro = ft.Container(
-            content=ft.Column(
-                controls=[Nome, Tel, Email, sexo, adm, senha, cadastrar_btn],
-                alignment=ft.MainAxisAlignment.CENTER,
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                spacing=20
-            ),
-            bgcolor=ft.colors.WHITE,
-            width=595,
-            height=670,
-            alignment=ft.alignment.center
-        )
-                
-        principal.content = ft.Row(
-            controls=[img_container, cadastro],
             alignment=ft.MainAxisAlignment.CENTER,  # Centralizando os contêineres na tela
             expand=True
         )
